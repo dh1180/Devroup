09c7c605935ac9a140e6da9cd30dc17054b696e4
항붕이 <mjdh1936@gmail.com>
2023-12-19 13:24:53 +0000

    fix: 로그인을 하지 않고 게시글에 댓글 작성 시 The view community.views_comment.comment_create didn't return an HttpResponse object. It returned None instead. 오류가 발생하는 현상 수정

--- community/views_comment.py
+++ community/views_comment.py
@@ -8,8 +8,8 @@ from django.contrib import messages
 
 
 def comment_create(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.user.is_authenticated and request.method == 'POST':
-        post = get_object_or_404(Post, pk=pk)
         comment = Comment()
         comment.post = post
         comment.author = request.user
@@ -17,6 +17,7 @@ def comment_create(request, pk):
         comment.save()
         return redirect('community:post_detail', pk=post.pk)
     messages.warning(request, "로그인 후 다시 시도해 주세요.")
+    return redirect('community:post_detail', pk=post.pk)
 
 
 def comment_delete(request, post_pk, comment_pk):
