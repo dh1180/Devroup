d63cad3e3dc3266dc0cc7b267cbb1fba3507b9ca
항붕이 <mjdh1936@gmail.com>
2023-12-24 14:51:10 +0000

    style: 로고 이미지 변경,  post 디폴트 썸네일 이미지 변경
    다크모드로 웹사이트에 접속하면 이미지가 부자연스러워져 로고, post 디폴트 썸네일 이미지를 모두 transparent 이미지로 변경함

--- community/views.py
+++ community/views.py
@@ -53,9 +53,9 @@ def post_create(request):
             post.save()
             return redirect('community:post_detail', pk=post.pk)
         else:
-            default_image_path = os.path.join(settings.MEDIA_ROOT, 'logo', 'Devroup_default.png')
+            default_image_path = os.path.join(settings.MEDIA_ROOT, 'logo', 'Devroup_default_transparent.png')
             with open(default_image_path, 'rb') as default_image_file:
-                post.image.save('Devroup_default.png', default_image_file, save=True)
+                post.image.save('Devroup_default_transparent.png', default_image_file, save=True)
                 post.save()
                 return redirect('community:post_detail', pk=post.pk)
     return render(request, 'community/post_create.html')
